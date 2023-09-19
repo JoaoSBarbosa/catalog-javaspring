be726b28ae3623cc316660a881459e4e739acd3e
João Barbosa <contato.jsbarbosa@gmail.com>
2023-09-18 22:11:02 -0300

    Backend project created

---
+++ backend/src/main/java/br/com/joaosbarbosa/jbcatalog/JbcatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaosbarbosa.jbcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbcatalogApplication.class, args);
+	}
+
+}
