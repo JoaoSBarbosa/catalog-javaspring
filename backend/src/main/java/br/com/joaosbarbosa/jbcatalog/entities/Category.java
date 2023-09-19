42d6c6613fea051f3ef91d636315a53ca68347b6
João Barbosa <contato.jsbarbosa@gmail.com>
2023-09-18 22:32:31 -0300

    Entitie Category

---
+++ backend/src/main/java/br/com/joaosbarbosa/jbcatalog/entities/Category.java
@@ -0,0 +1,51 @@
+package br.com.joaosbarbosa.jbcatalog.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "CATEGORIA_TB")
+public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cat_id")
+    private Long id;
+    @Column(name = "cat_name")
+    private String name;
+    public Category(){}
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId()) && Objects.equals(getName(), category.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+}
