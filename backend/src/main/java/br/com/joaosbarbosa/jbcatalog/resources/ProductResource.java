00c3a95129b998184ac0523ca05039515c47441c
João Barbosa <contato.jsbarbosa@gmail.com>
2023-09-28 21:33:20 -0300

    README

--- backend/src/main/java/br/com/joaosbarbosa/jbcatalog/resources/ProductResource.java
+++ backend/src/main/java/br/com/joaosbarbosa/jbcatalog/resources/ProductResource.java
@@ -34,7 +34,7 @@ public class ProductResource {
     @GetMapping
     public ResponseEntity<Page<ProductDTO>> searchAllPages(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage,
             @RequestParam(value = "direction", defaultValue = "ASC") String direction,
             @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
     ){
