d631b981c3ae5c780aba9ac598c77470925f9613
João Barbosa <contato.jsbarbosa@gmail.com>
2023-10-10 20:46:28 -0300

    Implemented the class User and mapping with the Role class

--- backend/src/main/java/br/com/joaosbarbosa/jbcatalog/entities/Role.java
+++ backend/src/main/java/br/com/joaosbarbosa/jbcatalog/entities/Role.java
@@ -8,6 +8,8 @@ import java.util.Objects;
 @Entity
 @Table(name = "tb_role")
 public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
