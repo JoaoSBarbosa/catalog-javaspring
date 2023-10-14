f32b4c2bef563853e54534edf6d29aeb96360b61
João Barbosa <contato.jsbarbosa@gmail.com>
2023-10-14 19:29:22 -0300

    UserDetails

--- backend/src/main/java/br/com/joaosbarbosa/jbcatalog/entities/User.java
+++ backend/src/main/java/br/com/joaosbarbosa/jbcatalog/entities/User.java
@@ -1,10 +1,16 @@
 package br.com.joaosbarbosa.jbcatalog.entities;
 
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -21,7 +27,7 @@ import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails,Serializable {
 
     private static final long serialVersionUID = 1L;
 
@@ -89,12 +95,41 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     @Override
