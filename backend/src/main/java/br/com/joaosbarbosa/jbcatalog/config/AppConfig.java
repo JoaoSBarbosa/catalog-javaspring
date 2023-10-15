53ca96771bc133c0e2b6ce99c3e14ff521efbdf9
João Barbosa <contato.jsbarbosa@gmail.com>
2023-10-15 10:00:43 -0300

    Implementing OAuth2 authorization server

--- backend/src/main/java/br/com/joaosbarbosa/jbcatalog/config/AppConfig.java
+++ backend/src/main/java/br/com/joaosbarbosa/jbcatalog/config/AppConfig.java
@@ -3,11 +3,28 @@ package br.com.joaosbarbosa.jbcatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig  {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    //Objetos para ler token jwt(ler,criar,acessar)
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        //instanciando objeto
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        //registrando a chave do token
+        tokenConverter.setSigningKey("my-jwt-secret");
+        //retornando o token
+        return tokenConverter;
+    }
+    @Bean
+    public JwtTokenStore tokenStore(){
+        return new JwtTokenStore(accessTokenConverter());
     }
 }
---
+++ backend/src/main/java/br/com/joaosbarbosa/jbcatalog/config/AuthorizationServerConfig.java
@@ -0,0 +1,48 @@
+package br.com.joaosbarbosa.jbcatalog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired BCryptPasswordEncoder passwordEncoder;
+    @Autowired JwtAccessTokenConverter accessTokenConverter;
+    @Autowired JwtTokenStore tokenStore;
+    @Autowired AuthenticationManager authenticationManager;
+
+    // Configura as permissões de acesso aos endpoints de geração e validação de tokens
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+
+    // Configura as informações do cliente (nome, senha, escopos, tipo de concessão, etc.)
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("jbcatalog") // Nome da aplicação que solicitará acesso
+                .secret(passwordEncoder.encode("jbcatalog123")) // Senha para acessar a aplicação
+                .scopes("read", "write") // Escopos de acesso (leitura, escrita, etc.)
+                .authorizedGrantTypes("password") // Padrão de concessão de token
+                .accessTokenValiditySeconds(86400); // Tempo de validade do token (24 horas)
+    }
+
+    // Configura os endpoints para autenticação e o formato do token
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager) // Gerenciador de autenticação
+                .tokenStore(tokenStore) // Armazenamento do token
+                .accessTokenConverter(accessTokenConverter); // Conversor de token
+    }
+}
