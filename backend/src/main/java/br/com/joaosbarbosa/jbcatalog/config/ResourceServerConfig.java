9820652d72ec0751ddbb74da2bcaa6312ee6423f
João Barbosa <contato.jsbarbosa@gmail.com>
2023-10-16 20:27:58 -0300

    Liberando acesso para h2 em ambiente de teste

--- backend/src/main/java/br/com/joaosbarbosa/jbcatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/br/com/joaosbarbosa/jbcatalog/config/ResourceServerConfig.java
@@ -2,12 +2,15 @@ package br.com.joaosbarbosa.jbcatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -16,8 +19,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private JwtTokenStore tokenStore;
 
+    //Ambiente de execução da aplicação
+    @Autowired
+    private Environment env;
+
     // Define rotas públicas que não precisam de autenticação
-    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] PUBLIC = {"/oauth/token","/h2-console/**"};
     // Define rotas acessíveis para usuários com perfil OPERATOR ou ADMIN
     private static final String[] OPERATOR_OR_ADMIN = {"/categorias/**", "/produtos/**"};
     // Define rotas acessíveis apenas para usuários com perfil ADMIN
@@ -32,6 +39,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     // configuração das rotas
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //comando para liberar o H2
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()//Para rotas PUBLIC, está liberado acessos
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()// Permite acesso liberado de leitura(GET)
