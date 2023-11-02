20b5fbb00c79c5612196fe43de6f85506052b139
João Barbosa <contato.jsbarbosa@gmail.com>
2023-11-02 19:10:52 -0300

    start of main structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import "./assets/styles/custom.scss";
 import "./App.css";
 import { NavBar } from "./components/Navbar";
+import { Home } from "pages/Home";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <Home />
     </>
   );
 }
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,28 @@
+import { ReactComponent as MainImage } from "assets/images/catalogo.svg";
+import { NavBar } from "components/Navbar";
+export const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <section className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <h1>Explore nossa gama de Produtos Tecnológicos</h1>
+            <p>
+              Descubra uma ampla variedade de produtos tecnológicos de última
+              geração em nosso catálogo. De dispositivos inovadores a acessórios
+              essenciais, oferecemos soluções para atender a todas as suas
+              necessidades tecnológicas. Clique no botão abaixo para explorar
+              nossos produtos e encontrar o que se encaixa perfeitamente em seu
+              estilo de vida digital.
+            </p>
+          </div>
+
+          <div className="home-image-container">
+            <MainImage />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
