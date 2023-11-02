edb430ddd9dda018354334f2f6366669f5148efe
João Barbosa <contato.jsbarbosa@gmail.com>
2023-11-02 17:40:38 -0300

    Navbar styles

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,8 +1,9 @@
 import "./styles.css";
+import "bootstrap/js/src/collapse.js";
 
 export const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-md  bg-primary nav-main">
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary nav-main">
       <div className="container-fluid">
         <a href="/" className="nav-logo">
           <img
@@ -12,7 +13,19 @@ export const NavBar = () => {
           />
         </a>
 
-        <div className="collapse navbar-collapse">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#cyberwolfstore-navbar"
+          aria-controls="cyberwolfstore-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="cyberwolfstore-navbar">
           <ul className="navbar-nav offset-md-2 nav-container-menu">
             <li>
               <a href="/" className="active">
