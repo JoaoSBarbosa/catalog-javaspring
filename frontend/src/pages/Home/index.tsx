e338899294b0fafa91eaad76d1910d76453cca83
João Barbosa <contato.jsbarbosa@gmail.com>
2023-11-03 23:15:02 -0300

    Finished, the style is the responsiveness of the home

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -9,17 +9,14 @@ export const Home = () => {
       <section className="home-container ">
         <div className="home-card">
           <div className="home-content-container">
-            <h1>Explore nossa gama de Produtos Tecnológicos</h1>
-            <p className="home-content-description">
-              Descubra uma ampla variedade de produtos tecnológicos de última
-              geração em nosso catálogo. De dispositivos inovadores a acessórios
-              essenciais, oferecemos soluções para atender a todas as suas
-              necessidades tecnológicas. Clique no botão abaixo para explorar
-              nossos produtos e encontrar o que se encaixa perfeitamente em seu
-              estilo de vida digital.
-            </p>
-
-            <ButtonIcon value="Explorar Catálogo"/>
+            <div>
+              <h1>Explore nossa gama de Produtos Tecnológicos</h1>
+              <p className="home-content-description">
+                De dispositivos inovadores a acessórios essenciais, oferecemos
+                soluções para atender a todas as suas necessidades tecnológicas.
+              </p>
+            </div>
+            <ButtonIcon value="Explorar Catálogo" />
           </div>
 
           <div className="home-image-container">
