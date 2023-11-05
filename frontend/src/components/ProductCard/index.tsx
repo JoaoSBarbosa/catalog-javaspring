51eae1f5a011eaffbfb198c989ac83c3f35ff212
João Barbosa <contato.jsbarbosa@gmail.com>
2023-11-05 11:06:45 -0300

    Finalized basic catalog layout - grid bootstrap

--- frontend/src/components/ProductCard/index.tsx
+++ frontend/src/components/ProductCard/index.tsx
@@ -1,17 +1,19 @@
 import { NavBar } from "components/Navbar";
 import "./styles.css";
 import cameraFull from "../../assets/images/camera-full.png";
+import foneMouse from "assets/images/fone-mause.png";
 import { ProductPrice } from "components/ProductPrice";
+
 export const ProductCard = () => {
   return (
     <div className="base-card product-card">
       <div className="card-top-container">
-        <img src={cameraFull} alt="camera" style={{ height: "360px" }} />
+        <img src={foneMouse} alt="camera" style={{ height: "360px" }} />
       </div>
 
       <div className="card-bottom-container">
-        <h6>Camera digital</h6>
-        <ProductPrice price="3.500,00" />
+        <h6>Kit Pró - Headset Wolftech + Teclado Pró Wolf + Mouse Pró Alfa</h6>
+        <ProductPrice price="550,00" />
       </div>
     </div>
   );
