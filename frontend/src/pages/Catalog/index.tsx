49c6d1d25e86150a76def45dd14f2f11060e47d3
João Barbosa <contato.jsbarbosa@gmail.com>
2023-11-11 01:08:08 -0300

    Loader

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -13,10 +13,12 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 // ---- Styles ----
+import CatalogMagic from "./loader"; // Importa o componente de loader
 import "./styles.css";
 
 export const Catalog = () => {
     const [page, setPage] = useState<SpringPage<Product>>();
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const params: AxiosParams = {
@@ -26,35 +28,33 @@ export const Catalog = () => {
                 page: 0,
                 size: 12
             },
-        }
+        };
+
         axios(params)
             .then(response => {
                 setPage(response.data);
+            })
+            .finally(() => {
+                setLoading(false);
             });
-    }, [])
-
+    }, []);
 
     return (
         <div className="container my-4 catalog-container">
             <div className="row catalog-title-container">
                 <h1>Catálogo de produtos</h1>
             </div>
-            <div className="row ">
+            <div className="row">
 
-                {page?.content.map((product) => {
-                        return (
-                            <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
-                                <Link to={`/products/${product.id}`}>
-                                    <ProductCard product={product}/>
-                                </Link>
-                            </div>
-                        )
-                    }
-                )}
-
+                {loading ? <CatalogMagic backgroundColor={"#bbb"}/> : (page?.content.map((product) => (
+                    <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
+                        <Link to={`/products/${product.id}`}>
+                            <ProductCard product={product}/>
+                        </Link>
+                    </div>
+                )))}
             </div>
-
-            <div className="row ">
+            <div className="row">
                 <Pagination/>
             </div>
         </div>
