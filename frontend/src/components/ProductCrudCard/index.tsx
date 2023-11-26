ae8fa1e009189bbea5944949b497c028b9c1cf32
João Barbosa <contato.jsbarbosa@gmail.com>
2023-11-26 10:58:30 -0300

    initial product list layout and design

---
+++ frontend/src/components/ProductCrudCard/index.tsx
@@ -0,0 +1,30 @@
+import { NavBar } from "components/Navbar";
+import "./styles.css";
+import cameraFull from "../../assets/images/camera-full.png";
+import foneMouse from "assets/images/fone-mause.png";
+import { ProductPrice } from "components/ProductPrice";
+import {Product} from "../../types/Product";
+import {Category} from "../../types/Category";
+
+type cardProps ={
+    product: Product;
+}
+
+export const ProductCrudCard = ({product}: cardProps) => {
+  return (
+    <div className="base-card product-card">
+      <div className="card-top-container">
+        <img
+            src={product.imgUrl?product.imgUrl:"https://uploaddeimagens.com.br/images/004/656/445/full/logo-cyber2.png?1699207931"}
+            alt={product.name}
+            style={{ height: "360px" }}
+        />
+      </div>
+
+      <div className="card-bottom-container">
+        <h6>{product.name}</h6>
+        <ProductPrice price={product.price} />
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,8 +1,55 @@
+import {ProductCrudCard} from "../../../../components/ProductCrudCard";
 
-export const List =()=>{
-    return(
-       <div>
-           <h1>Tela da lista</h1>
-       </div>
+import "./styles.css";
+import {Link} from "react-router-dom";
+
+export const List = () => {
+    const product = {
+        "id": 5,
+        "name": "Rails for Dummies",
+        "description": "Inicie sua jornada no desenvolvimento web com \"Rails for Dummies\". Aprenda os conceitos essenciais de forma fácil e divertida.",
+        "price": 100.99,
+        "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg",
+        "date": "2020-07-14T10:00:00Z",
+        "categories": [
+            {
+                "id": 2,
+                "name": "Eletrônicos"
+            }
+        ]
+    }
+    return (
+
+        <>
+            <div className={"product-crud-bar-container"}>
+                <Link to={"/admin/products/create"}>
+                    <button className={"btn btn-primary btn-crud-add"}>ADICIONAR</button>
+                </Link>
+                <div className={"base-card product-search-container"}>
+                    Barra de busca
+                </div>
+            </div>
+
+            <div className={"row"}>
+                <div className={"col-sm-6 col-md-12"}>
+                    <ProductCrudCard product={product}/>
+                </div> <div className={"col-sm-6 col-md-12"}>
+                    <ProductCrudCard product={product}/>
+                </div> <div className={"col-sm-6 col-md-12"}>
+                    <ProductCrudCard product={product}/>
+                </div> <div className={"col-sm-6 col-md-12"}>
+                    <ProductCrudCard product={product}/>
+                </div>
+                <div className={"col-sm-6 col-md-12"}>
+                    <ProductCrudCard product={product}/>
+                </div>
+                <div className={"col-sm-6 col-md-12"}>
+                    <ProductCrudCard product={product}/>
+                </div>
+
+            </div>
+
+
+        </>
     )
 }
