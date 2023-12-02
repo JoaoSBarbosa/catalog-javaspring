8deee7aa5b3fdb33bdf2a7c321383a2c5b75afee
João Barbosa <contato.jsbarbosa@gmail.com>
2023-12-01 21:51:17 -0300

    Initial styles for product crud form

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -8,17 +8,30 @@ export const Form = () => {
                 <h1 className={"product-crud-form-card-title"}>Dados do produto</h1>
 
                 <form action="" className={"product-crud-form-card-form"}>
-                    <div className={"row"}>
-                        <div className={"col-lg-6"}>
-                            <input type="text" className={"form-control base-input"}/>
-                            <input type="text" className={"form-control base-input"}/>
-                            <input type="text" className={"form-control base-input"}/>
+                    <div className={"row product-crud-inputs-container"}>
+                        <div className={"col-lg-6 product-crud-column-left-container"}>
+                            <div className={"product-crud-input"}>
+                                <input type="text" className={"form-control base-input"}/>
+                            </div>
+                            <div className={"product-crud-input"}>
+                                <input type="text" className={"form-control base-input"}/>
+                            </div>
+                            <div className={"product-crud-input"}>
+                                <input type="text" className={"form-control base-input"}/>
+                            </div>
+
                         </div>
                         <div className={"col-lg-6"}>
-                            <textarea name="" rows={10} className={"form-control base-input"}></textarea>
+                            <textarea
+                                name=""
+                                rows={5}
+                                className={"form-control"}
+                            >
+
+                            </textarea>
                         </div>
                     </div>
-                    <div className={"product-crud-form-card-buttons"}>
+                    <div className={"product-crud-buttons-container"}>
                         <button className={"btn btn-outline-danger"}>Cancelar</button>
                         <button className={"btn btn-success"}>Salvar</button>
                     </div>
