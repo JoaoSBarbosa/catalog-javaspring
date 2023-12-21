657a75b670660fa6644d16c3ac92932e9a44d902
João Barbosa <contato.jsbarbosa@gmail.com>
2023-12-20 21:42:13 -0300

    Layout and initial styles of product filters - input field name

--- frontend/src/components/ProductFilter/index.tsx
+++ frontend/src/components/ProductFilter/index.tsx
@@ -1,8 +1,33 @@
 import "./styles.css"
+import {MagnifyingGlass} from "@phosphor-icons/react";
 export const ProductFilter = () => {
     return (
         <div className={"base-card product-search-container"}>
-            Buscar produto por nome...
+
+            <form action="" className={"product-filter-form-container"}>
+                <div className={"product-filter-name-container"}>
+                    <input
+                        type="text"
+                        className={""}
+                        placeholder={"Nome do produto"}
+                    />
+                    <MagnifyingGlass size={22} color="#aaaaaa" weight="light" />                </div>
+
+                <div className={"product-filter-bottom-container"}>
+                    <div className={"product-filter-category-container"}>
+                        <select >
+                            <option value="livros">
+                                Livros
+                            </option>
+                            <option value={"brinquedos"}>
+                                Brinquedos
+                            </option>
+                        </select>
+                    </div>
+                    <button className={"btn btn-outline-success"}>Limpar</button>
+                </div>
+
+            </form>
         </div>
     )
 }
